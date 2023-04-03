7bbde5dbfa4c7e4d0dfa048cd5a7eb3d97f3cdd5
kirilovaiva8 <129675234+kirilovaiva8@users.noreply.github.com>
2023-04-03 22:15:57 +0300

    Пирамида в turtle!

---
+++ pyramid succesful.py
@@ -0,0 +1,29 @@
+import turtle
+ 
+t = turtle
+t.speed(0.6)
+for i in range(2) :
+    t.forward(100)
+    t.left(70)
+    t.forward(100)
+    t.left(110)
+
+t.forward(50)
+t.left(70)
+t.forward(50)
+t.left(20)
+t.forward(100)
+t.left(148)
+t.forward(65)
+t.left(12.5)
+t.forward(97.9)
+t.left(175)
+t.forward(162.9)
+t.right(143.1)
+t.forward(151.5)
+t.left(147.6)
+t.forward(100)
+t.left(71.3)
+t.forward(85.5)
+t.right(90)
+t.forward(50000)
